b665371a0c1574fbb2de3b6640abb7b4a1a135be
Chris Dennis <chrisdennis0913@gmail.com>
2012-02-12 13:18:25 -0500

    Event and Tivoo input casses, kinda

---
+++ src/Event.java
@@ -0,0 +1,29 @@
+import java.sql.Time;
+import java.util.GregorianCalendar;
+
+public class Event
+{
+    private String subject;
+    private GregorianCalendar start;
+    private GregorianCalendar end;
+    // deal with all day events
+    private String description;
+    private String location;
+    private String priority;
+    private boolean privacy;
+    private boolean reminderOnOff;
+    private GregorianCalendar reminder;
+    private String sensitivity;
+    private String showTimeAs;
+    
+    
+    public void returnTime(){
+        start.set(year, month, date, hourOfDay, minute);
+        gregorian.
+        Time.UTC()
+        calendar.date;
+        Date.
+    }
+    public Event(String subject, int Month, )
+
+}
